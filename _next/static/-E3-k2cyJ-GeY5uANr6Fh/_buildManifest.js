1815980b638adc82b66f699440e9cfccbd6982dc
caiocampos <20524962+caiocampos@users.noreply.github.com>
2024-10-19 01:16:10 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@c497e6ad45145a1a0cdd4a58fb487e0f616c69e7 🚀

--- _next/static/-E3-k2cyJ-GeY5uANr6Fh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/727f57db-57ff7eefb77b3218.js","static/chunks/pages/index-c3c00622ec405b80.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/28seDLIrEkndXQCckzJnH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/727f57db-31aead415e3409cc.js","static/chunks/pages/index-c13cf8df3325bb3e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
