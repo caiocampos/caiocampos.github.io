e32f85e9285fd71b419a4e50944877707290b0e8
caiocampos <20524962+caiocampos@users.noreply.github.com>
2024-10-22 22:36:44 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@f9e137082eafbc9214d8e03d04f0f6f1966f361f 🚀

--- _next/static/ujxbXDuayTfuFaX6kuPV8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/727f57db-7ac65848f811422d.js","static/chunks/pages/index-2c2c408f5c4e9b66.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
