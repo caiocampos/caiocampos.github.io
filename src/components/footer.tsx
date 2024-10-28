62a28a70a09df2d9562790be65ec400c0ac9193d
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-10-28 18:38:35 -0300

    fix size of footer disclaimer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ export const Footer = ({ disclaimer }: FooterProps): JSX.Element => {
           <LogoGithubIcon size="small" />
         </div>
         <div className="absolute flex items-center h-full align-middle right-0 top-0">
-          <div className="max-w-32 p-2 sm:max-w-44 text-white text-sm sm:text-base">{disclaimer}</div></div>
+          <div className="max-w-32 p-2 sm:max-w-52 text-white text-sm sm:text-base">{disclaimer}</div></div>
       </div>
     </a>
   );
