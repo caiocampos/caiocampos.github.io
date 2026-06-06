92b8d0b27863fe1d9179c47c2b41a172556b97f0
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-06-06 21:28:28 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@0e94023d0353a3584181a3a36ce85c64849d9c06 🚀

---
+++ _next/static/chunks/1o9-g9noo56fb.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0wkwzbe-6lhi-.js",
+  "static/chunks/3w2n2in3gii6j.js",
+  "static/chunks/43nfw1dfyhsfj.js",
+  "static/chunks/2rfe5twqftj08.css",
+  "static/chunks/turbopack-40400yxe7wu6r.js"
+])
---
+++ _next/static/jd_bOv3oyNGiAbDOU0M2p/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/31g-cndi11xem.js"
+  ],
+  "/_error": [
+    "static/chunks/3fia3b1cpei68.js"
+  ],
+  "/home/[language]": [
+    "static/chunks/16-5gml1yhjus.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/home/[language]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
