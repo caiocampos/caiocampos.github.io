ccf5e83dcbd673334a6a9c51a569b0799b4df0b7
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-07-01 01:18:09 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@09e897a59d80f5e12624c431c5c0c879b40f01a6 🚀

--- _next/static/03T25mqOaENkWQ3Jpq-4M/_buildManifest.js
+++ _next/static/03T25mqOaENkWQ3Jpq-4M/_buildManifest.js
@@ -6,7 +6,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/0ovu6__9ewo4v.js"
   ],
   "/home/[language]": [
-    "static/chunks/3l2r4exlr1tpm.js"
+    "static/chunks/3z3-1r2oqafg2.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- _next/static/chunks/3z3-1r2oqafg2.js
+++ _next/static/chunks/3z3-1r2oqafg2.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/home/[language]", [
   "static/chunks/1wrdf7dhi4e4p.js",
   "static/chunks/35ofw5uw80608.js",
-  "static/chunks/0ebavb24x2p0t.js",
+  "static/chunks/1sxcydk1fieau.js",
   "static/chunks/2cbq38v59vjds.js",
-  "static/chunks/turbopack-2os4kkyy5kc1_.js"
+  "static/chunks/turbopack-16n61russuwzz.js"
 ])
