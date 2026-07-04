54183e6b43ca6a0b14851b28a2abfa2911e8b587
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-07-04 18:40:25 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@55f3c0c4de3f0104aa4045fcbdd46c0ed090b577 🚀

--- _next/static/chunks/44i-___kqh141.js
+++ _next/static/chunks/44i-___kqh141.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/1s1stvzgbtbc_.js",
   "static/chunks/3wwvr3pjrbomg.js",
   "static/chunks/35ofw5uw80608.js",
-  "static/chunks/37__r1w_b_9p5.css",
-  "static/chunks/turbopack-1vm06vxk_f7af.js"
+  "static/chunks/27jkgq6_e17c4.css",
+  "static/chunks/turbopack-3byix62oqr046.js"
 ])
