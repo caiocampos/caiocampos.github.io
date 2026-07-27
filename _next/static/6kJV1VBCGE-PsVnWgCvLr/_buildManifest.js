aef88cfe1e06d76668abf46cab52bb3b850e2f8f
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-07-27 13:02:18 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@4e315f7764f1ea76e57436951228cd4bc47cccc5 🚀

--- _next/static/6kJV1VBCGE-PsVnWgCvLr/_buildManifest.js
+++ _next/static/6kJV1VBCGE-PsVnWgCvLr/_buildManifest.js
@@ -6,7 +6,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/1ybhr1o3iv7pw.js"
   ],
   "/home/[language]": [
-    "static/chunks/27flquum-pe8i.js"
+    "static/chunks/0gy2_9khrsq_8.js"
   ],
   "__rewrites": {
     "afterFiles": [],
