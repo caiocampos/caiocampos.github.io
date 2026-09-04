c6fc07d42de4bc9ae15df9f9f3aad7a948f970d5
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-09-04 11:12:28 +0000

    Deploying to master from @ caiocampos/caiocampos.github.io@31fb4e515d47b4f1cdbc723ac4818902c7561c2d 🚀

--- _next/static/OUwg__NBNzehZt9siqMoq/_buildManifest.js
+++ _next/static/OUwg__NBNzehZt9siqMoq/_buildManifest.js
@@ -1,12 +1,12 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/1rv7xl8w3w-am.js"
+    "static/chunks/0uytqgqf651cw.js"
   ],
   "/_error": [
-    "static/chunks/07ww-iwjh90-m.js"
+    "static/chunks/2_4t3okmarr81.js"
   ],
   "/home/[language]": [
-    "static/chunks/20gj9y12d5m9w.js"
+    "static/chunks/0b3sozfig-ky0.js"
   ],
   "__rewrites": {
     "afterFiles": [],
